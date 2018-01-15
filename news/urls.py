253a4a1d50057f70c9d75ac5ac424b0f311b0313
david mark <markkariuki17@gmail.com>
2018-01-15 12:06:55 +0300

    add the first tempate

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url,include
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^news/',include('news.urls'))
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+    url('^today/$',views.news_of_day,name='newsToday')
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
+]
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from django.http  import HttpResponse, Http404
+import datetime as dt
+
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
+
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+
+
+def past_days_news(request,past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+     except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
