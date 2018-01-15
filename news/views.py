932b7243622990293501fb0b0d0ca161a586df72
david mark <markkariuki17@gmail.com>
2018-01-15 12:21:04 +0300

    add the h1 text

--- news/views.py
+++ news/views.py
@@ -5,7 +5,7 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
 
 def news_of_day(request):
     date = dt.date.today()
@@ -24,7 +24,7 @@ def past_days_news(request,past_date):
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
-     except ValueError:
+    except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
 
